d6633e2c5ebd1ded36da1572020a40076fa11678
Kyle DeTella <kdetella@gmail.com>
2017-07-07 09:26:06 -0500

    Prettify

--- server.js
+++ server.js
@@ -3,20 +3,22 @@ const bodyParser = require('body-parser')
 const runLogic = require('./src/runLogic')
 
 const app = express()
-const {PORT = 3022} = process.env
+const { PORT = 3022 } = process.env
 
 app.use(bodyParser.json())
 
-app.post('/', ({body: {event_type, data}, hostname}, res) => {
+app.post('/', ({ body: { event_type, data }, hostname }, res) => {
   console.log(`[${hostname}]: "${event_type}" webhook received from Init.ai`)
 
   if (event_type === 'LogicInvocation') {
     runLogic(data)
       .then(() => console.log('LogicInvocation success!'))
-      .catch((error) => console.log('LogicInvocation Error:\n', error))
+      .catch(error => console.log('LogicInvocation Error:\n', error))
   }
 
   res.sendStatus(200)
 })
 
-app.listen(PORT, () => console.log(`Webhook server is running on port ${PORT}!`))
+app.listen(PORT, () =>
+  console.log(`Webhook server is running on port ${PORT}!`)
+)
